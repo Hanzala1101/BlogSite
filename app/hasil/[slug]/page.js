ab74c10dca3b16d12433c093dc9f377dfd8f20c7
Hanzala1101 <hanzala.612006.it@mhssce.ac.in>
2024-02-13 18:19:19 +0530

    error of read blog

--- app/hasil/[slug]/page.js
+++ app/hasil/[slug]/page.js
@@ -4,7 +4,7 @@ const getBlogs = async (id) => {
   // console.log("id",id)
   
     try {
-      const res = await fetch(`httpa://blog-zaara/api/blog/${id}`, {
+      const res = await fetch(`https://blog-zaara.vercel.app/api/blog/${id}`, {
         cache: "no-store",
       });
   
